328d6daf1634d02f01ec588321ec024bb3b7d1ee
Kenz <yutthagarn.ken@gmail.com>
2019-09-09 15:47:51 +0700

    first commit

---
+++ src/main/java/com/example/springbootoauth2resourceserver/config/WebSecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.example.springbootoauth2resourceserver.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+}
